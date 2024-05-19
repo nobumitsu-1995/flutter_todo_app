1e97df60e07ea319162813b4831223fa4a1763ce
nobumitsu_masugata <ys.training220208@gmail.com>
2024-05-19 22:28:36 +0900

    feat: TODO作成フォームのUIを作成

--- lib/pages/todo_create_edit_page.dart
+++ lib/pages/todo_create_edit_page.dart
@@ -8,12 +8,44 @@ class TodoCreateEditPage extends StatefulWidget {
 }
 
 class _TodoCreateEditPageState extends State<TodoCreateEditPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController detailController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('TODO作成')
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.8,
+          alignment: Alignment.center,
+          child: Column(
+            children: [
+              const SizedBox(height: 40),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: 'タイトル'
+                ),
+                controller: titleController,
+              ),
+              const SizedBox(height: 20),
+              TextField(
+                decoration: const InputDecoration(
+                  labelText: '詳細'
+                ),
+                controller: detailController,
+              ),
+              const SizedBox(height: 40),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('新規作成')
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
